5e9dc222d1abd320afa2a09d5cc78c0c8e95f836
milirezai <miladrezaix@gmail.com>
2026-08-01 20:52:50 +0330

    resolve bux in swagger

--- routes/Api/V1/V1.php
+++ routes/Api/V1/V1.php
@@ -3,8 +3,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\V1Controller;
+use App\Http\Controllers\Api\V1\Auth\RegisterController;
+
+
+
 
 // swagger
 
 Route::get('/',[V1Controller::class,'index']);
 
+// auth
+
+Route::prefix('auth')->group(function (){
+    Route::post('/',[RegisterController::class])
+});
