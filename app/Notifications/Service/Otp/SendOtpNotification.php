7992cf531bd534afd8b83cf70fb23ba5fdf19203
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-08-03 15:20:43 +0330

    feat: add SendOtpNotification

---
+++ app/Notifications/Service/Otp/SendOtpNotification.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications\Service\Otp;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class SendOtpNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public string $otpCode)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject("TicketFlow OTP verification")
+            ->greeting("Hello {$notifiable->first_name} {$notifiable->last_name}")
+            ->line("Your code is:")
+            ->line("{$this->otpCode}")
+            ->line("This code will expire in a few minutes.");
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
