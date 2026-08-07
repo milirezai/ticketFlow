a464a49d3307f451f988159a371bac80363fa5f3
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-08-07 16:37:21 +0330

    feat: add Role and Permission models

---
+++ app/Models/Access/Permission.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\Access;
+
+use App\Models\User\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Permission extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'description', 'status'];
+    protected $casts = ['status' => 'boolean'];
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
