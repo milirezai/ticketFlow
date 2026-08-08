8b853b1fc497ace05486ad9bcf1b84fe0b557ad7
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-08-08 13:56:24 +0330

    feat: add role and permission factories

--- app/Models/Access/Role.php
+++ app/Models/Access/Role.php
@@ -3,13 +3,14 @@
 namespace App\Models\Access;
 
 use App\Models\User\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasFactory;
 
     protected $fillable = [
         'name',
