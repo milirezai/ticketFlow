750e5d26d50d60c4a68bffd77c04c598fe56df78
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-08-12 13:46:09 +0330

    refactor: add id and slug to ticket category, priority and status resources

--- app/Http/Resources/Api/V1/Ticket/TicketStatusResource.php
+++ app/Http/Resources/Api/V1/Ticket/TicketStatusResource.php
@@ -15,7 +15,9 @@ class TicketStatusResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
+            'slug' => $this->slug,
             'description' => $this->description,
             'status' => $this->status,
             'created_at' => $this->created_at->isoFormat('dddd D MMMM Y'),
