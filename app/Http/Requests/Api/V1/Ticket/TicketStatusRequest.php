d4d7594515fbf1f6c8a0e6776a07ff121ec45e8e
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-08-12 11:44:10 +0330

    feat: add validation requests for ticket category, priority and status

---
+++ app/Http/Requests/Api/V1/Ticket/TicketStatusRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\Ticket;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class TicketStatusRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'min:2', 'max:255'],
+            'slug' => [
+                'sometimes',
+                'nullable',
+                'string',
+                'alpha_dash',
+                'max:255',
+                Rule::unique('ticket_statuses', 'slug')->ignore($this->route('ticket_status'))->whereNull('deleted_at'),
+            ],
+            'description' => ['nullable', 'string', 'max:500'],
+            'status' => ['sometimes', 'boolean'],
+        ];
+    }
+}
